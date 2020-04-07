8f7606b816b471bacbf3ac17a35f277acea69b5d
anndev68 <chk392013@gmail.com>
2020-04-07 15:40:23 +0900

    fetchを追加

--- basic/client/js/components/todo.js
+++ basic/client/js/components/todo.js
@@ -9,7 +9,10 @@ class Todo {
   mount() {
     const removeButton = this.element.querySelector('.todo-remove-button');
     removeButton.addEventListener('click', ()=>{
-      console.log(this);
+      let url = "http://localhost:3000/todo/" + this.props.id;
+      fetch(url, {method: 'DELETE'})
+        .then(()=>{})
+        .catch(()=>{})
     });
   }
 
