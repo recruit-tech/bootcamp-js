14ba1224dd45274c451dfcca7389fe8de54667ac
金子　翔麻 <01035870@CA6175.local>
2020-04-07 16:21:32 +0900

    feat: ToDo削除時のstate更新

--- basic/client/js/flux/index.js
+++ basic/client/js/flux/index.js
@@ -76,9 +76,13 @@ const reducer = async (prevState, { type, payload }) => {
       const url = 'http://localhost:3000/todo/' + payload
       try{
         const resp = await fetch(url, { method: 'DELETE' })
-        // @TODO
-      }catch(err) {
-        console.error(err)
+        const index = prevState.todoList.findIndex(todo => todo.id === payload)
+        if(index === -1) return
+        const nextTodoList = [...prevState.todoList]
+        nextTodoList.splice(index, 1)
+        return { todoList: nextTodoList, error: null }
+      } catch (err) {
+        return {...prevState, error: err}
       }
     }
     case CLEAR_ERROR: {
