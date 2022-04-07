a8bff6c61b52ac91a0ab9e98d00df011eaa4983f
penicillin0 <ayahitonakamura@gmail.com>
2022-04-07 14:50:36 +0900

    refactor

--- advanced/client/js/index.js
+++ advanced/client/js/index.js
@@ -23,37 +23,20 @@ const makeTodoItem = (id, todo_name, done) => {
   const liElement = document.createElement("li");
   liElement.className = "todo-item";
 
-  // ここからlabel
-  const labelElement = document.createElement("label");
-  labelElement.className = "todo-toggle__container";
-
-  const inputElement = document.createElement("input");
-  inputElement.className = "todo-toggle";
-  if (done) {
-    inputElement.value = "checked";
-  }
-  inputElement.setAttribute("data-todo-id", String(id));
-  inputElement.setAttribute("type", "checkbox");
-
-  const spanElement = document.createElement("span");
-  spanElement.className = "todo-toggle__checkmark";
-
-  labelElement.appendChild(inputElement);
-  labelElement.appendChild(spanElement);
-  // ここまでlabel
-
-  const divTodoNameElement = document.createElement("div");
-  divTodoNameElement.className = "todo-name";
-  divTodoNameElement.textContent = todo_name;
-
-  const divRemoveButton = document.createElement("div");
-  divRemoveButton.className = "todo-remove-button";
-  divRemoveButton.setAttribute("data-todo-id", String(id));
-  divRemoveButton.textContent = "x";
-
-  liElement.appendChild(labelElement);
-  liElement.appendChild(divTodoNameElement);
-  liElement.appendChild(divRemoveButton);
+  liElement.innerHTML = `
+      <label class="todo-toggle__container">
+        <input
+          data-todo-id="${id}"
+          type="checkbox"
+          class="todo-toggle"
+          value="checked"
+          ${done ? "checked" : ""}
+        />
+        <span class="todo-toggle__checkmark"></span>
+      </label>
+      <div class="todo-name">${todo_name}</div>
+      <div data-todo-id="${id}" class="todo-remove-button">x</div>
+    `;
 
   return liElement;
 };
